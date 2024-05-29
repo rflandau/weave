083973f24806e5f896f9e511caf50267d4b514a4
R Landau <rflandau@pm.me>
2024-05-29 15:44:14 -0400

    output arbitrary structs, limited by given columns, as CSV via weave

---
+++ weave.go
@@ -0,0 +1,94 @@
+/**
+ * The output module; weave consumes arbitrary structs, orchestrating them into
+ * a specified format, and returning the formatted string.
+ * TODO will this work? https://github.com/jszwec/csvutil
+ */
+package weave
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+/**
+ * Takes an array of arbitrary struct `st` and the *ordered* columns to include/exclude
+ * and returns a string containing the csv representation of the data contained
+ * therein.
+ * ! Returns the empty string if columns or st are empty
+ * ! the array of interfaces are expected to be structs with identical structure
+ * TODO incorporate exclude boolean to blacklist columns instead of assuming whitelist
+ * TODO allow column names to be case-insensitive
+ */
+func ToCSVHash(st []interface{}, columns []string) string {
+	// DESIGN:
+	// We have a list of column, ordered.
+	// We have a map of column names -> field index.
+	// For each struct s in the list of structs:
+	//	iterate through the list of columns and use the map to fetch the
+	//	column/field's values by index, building the csv token by token
+
+	if len(st) < 1 || len(columns) < 1 { // superfluous request
+		return ""
+	}
+
+	// deconstruct the first struct to validate requested columns
+	vals := reflect.ValueOf(st[0])
+	types := vals.Type()
+
+	var hdrBldr strings.Builder
+
+	// coordinate columns
+	// TODO keying the map on columns index would bring faster lookups
+	columnMap := make(map[string][]int, len(columns)) // column name -> recursive field indices
+	for i := range columns {
+		hdrBldr.WriteString(columns[i] + ",") // generate header
+		// map column names to their field indices
+		// if a name is not found, nil it so it can be skipped later
+		field, found := types.FieldByName(columns[i])
+		if !found {
+			columnMap[columns[i]] = nil
+			continue
+		}
+		columnMap[columns[i]] = field.Index
+	}
+	var hdr string = chomp(hdrBldr.String())
+
+	var csv strings.Builder // stores the actual data
+
+	for _, s := range st { // operate on each struct
+		csv.WriteString(stringifyStructCSV(s, columns, columnMap) + "\n")
+	}
+
+	return strings.TrimSpace(hdr + "\n" + csv.String())
+}
+
+// helper function for ToCSVHash
+// returns a string of a CSV row populated by the data in the struct that corresponds to the columns
+func stringifyStructCSV(s interface{}, columns []string, columnMap map[string][]int) string {
+	var row strings.Builder
+
+	// deconstruct the struct
+	structVals := reflect.ValueOf(s)
+
+	// search for each column
+	for _, col := range columns {
+		findices := columnMap[col]
+		// TODO iterate through the findices to get the final value in the event of struct embedding
+		if findices == nil {
+			// no matching field
+			// do nothing
+		} else {
+			// use field index to retrieve value
+			data := structVals.FieldByIndex(findices)
+			row.WriteString(fmt.Sprintf("%v", data))
+		}
+		row.WriteString(",") // append comma to token
+	}
+
+	return chomp(row.String())
+}
+
+func chomp(s string) string {
+	return strings.TrimSuffix(s, ",")
+}
---
+++ weave_test.go
@@ -0,0 +1,128 @@
+package weave
+
+import (
+	"testing"
+)
+
+type inner struct {
+	foo string
+}
+
+type outer struct {
+	inner
+	a        int
+	b        uint
+	c        float32
+	d        string
+	Exported float64
+}
+
+func TestToCSVHash(t *testing.T) {
+	type args struct {
+		st      []interface{}
+		columns []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"∃!c∃!r",
+			args{
+				st: []interface{}{
+					outer{
+						a:     10,
+						b:     0,
+						c:     5.0123,
+						d:     "D",
+						inner: inner{foo: "FOO"}}},
+				columns: []string{"a"}},
+			"a\n" + "10",
+		},
+		{"∀c∃!r, ordered as struct",
+			args{
+				st: []interface{}{
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        0,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145}},
+				columns: []string{
+					"a", "b", "c", "d", "foo", "Exported",
+				}},
+			"a,b,c,d,foo,Exported\n" + "10,0,5.0123,D,FOO,3.145",
+		},
+		{"∀c∃!r, ordered randomly",
+			args{
+				st: []interface{}{
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        0,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145}},
+				columns: []string{
+					"c", "foo", "Exported", "d", "a", "b",
+				}},
+			"c,foo,Exported,d,a,b\n" + "5.0123,FOO,3.145,D,10,0",
+		},
+		{"∀c5r, ordered randomly",
+			args{
+				st: []interface{}{
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        0,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145},
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        57,
+						b:        0,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145},
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        256,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145},
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        0,
+						c:        5.0123,
+						d:        "D",
+						Exported: 3.145},
+					outer{
+						inner:    inner{foo: "FOO"},
+						a:        10,
+						b:        0,
+						c:        5.0123,
+						d:        "D!",
+						Exported: 3.145}},
+				columns: []string{
+					"c", "foo", "Exported", "d", "a", "b",
+				}},
+			"c,foo,Exported,d,a,b\n" +
+				"5.0123,FOO,3.145,D,10,0\n" +
+				"5.0123,FOO,3.145,D,57,0\n" +
+				"5.0123,FOO,3.145,D,10,256\n" +
+				"5.0123,FOO,3.145,D,10,0\n" +
+				"5.0123,FOO,3.145,D!,10,0",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ToCSVHash(tt.args.st, tt.args.columns); got != tt.want {
+				t.Errorf("\n---ToCSVHash()---\n'%v'\n---want---\n'%v'", got, tt.want)
+			}
+		})
+	}
+}
